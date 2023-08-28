68933fda59b69071a31b7164aef0c07fb167246a
Erick <87406159+Neosowo@users.noreply.github.com>
2023-08-27 19:21:33 -0500

    Se agrega el proyecto

---
+++ Basic programs/hi.py
@@ -0,0 +1,9 @@
+#Para imprimir información en la consola, utilizamos la función 'print', la cual recibe un argumento que se coloca dentro de los paréntesis.
+#En el caso de la impresión de texto se usa:
+print("Esto es un texto en py")
+#En el caso de la impresión de variables se usa una variable ya declarada
+#Esto imprime cualquier tipo de dato que guarde esa variable
+x = "uwu"
+y = 13
+print(x)
+print(y)
