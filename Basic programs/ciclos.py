6a519979bf200c40ee121fca53e9b2dad5fa8a21
neosowo <erickyanezsantana@gmail.com>
2023-10-29 19:28:39 -0500

    update

---
+++ Basic programs/ciclos.py
@@ -0,0 +1,10 @@
+#Se repite mientras se cumpla la condición
+x = 10
+while(0 < x):
+    print(f"0 es menor que {x}")
+
+#Se puede incrementar con cada ciclo agregando la siguiente linea
+y = 0
+while(y < x):
+    print(f"0 es menor que {x}")
+    y=+1
